1db082314936cd70c6edb2fa728c674a36e85b3a
Alex Snyder <alystrife@gmail.com>
2021-12-19 18:42:03 -0500

    remove import

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
-const sequelize = require('sequelize');
 
 // get all users
 router.get('/', (req, res) => {
